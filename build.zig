f335937760fb41a71a0af8c6e54f48441321cd84
hectorgrey <hectorgrey223@protonmail.com>
2024-10-13 11:39:29 +0100

    fixed build script for 0.13.0

--- build.zig
+++ build.zig
@@ -3,9 +3,8 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "zigenoid",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .root_source_file = b.path("src/main.zig"),
+        .target = b.host,
     });
 
     exe.linkLibC();
