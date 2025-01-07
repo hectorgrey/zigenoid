beb2c38f5806d2bc01d5e8acbae69f9cef42d966
hectorgrey <hectorgrey223@protonmail.com>
2025-01-07 21:26:20 +0000

    placed level information into a state singleton

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,11 @@ const screen_height = 1080;
 
 const Paddle = struct { width: i32 = 200, height: i32 = 30, x: i32 = (screen_width / 2) - 100, y: i32 = screen_height - 60, vel_x: f64 = 0, left_bound: i32 = 30, right_bound: i32 = screen_width - 230, max_vel: f64 = 1000 };
 const Ball = struct { radius: i32 = 15, x: i32 = screen_width / 2, y: i32 = screen_height / 2, vel_x: f64 = 0, vel_y: f64 = 0 };
-const Block = struct { width: i32 = 200, height: i32 = 30, x: i32, y: i32, hp: i32 = 1 };
-const KeyState = struct { is_down: bool = false, was_down: bool = false };
+const Block = struct { width: i32 = 200, height: i32 = 30, x: i32 = 0, y: i32 = 0, hp: i32 = 1 };
+const KeyState = struct { is_down: bool = false };
 const KeyboardState = struct { left: KeyState = KeyState{}, right: KeyState = KeyState{} };
+const LevelLayout = struct { blocks: [8][8]i32 };
+const LevelState = struct { ball: Ball, paddle: Paddle, keyboard: KeyboardState, level: [8][8]Block };
 
 fn handle_event(event: sdl.SDL_Event, keyboard_state: *KeyboardState) bool {
     var should_close = false;
@@ -31,6 +33,15 @@ fn handle_event(event: sdl.SDL_Event, keyboard_state: *KeyboardState) bool {
         else => should_close = false,
     }
     return should_close;
+}
+
+fn init_game_state() LevelState {
+    const paddle = Paddle{};
+    const ball = Ball{ .vel_y = 10 };
+    const keyboard_state = KeyboardState{};
+    const blocks = [8][8]Block{ [8]Block{ Block{ .x = 55, .y = 30 }, Block{ .x = 285, .y = 30 }, Block{ .x = 515, .y = 30 }, Block{ .x = 745, .y = 30 }, Block{ .x = 975, .y = 30 }, Block{ .x = 1205, .y = 30 }, Block{ .x = 1435, .y = 30 }, Block{ .x = 1665, .y = 30 } }, [8]Block{ Block{ .x = 55, .y = 70 }, Block{ .x = 285, .y = 70 }, Block{ .x = 515, .y = 70 }, Block{ .x = 745, .y = 70 }, Block{ .x = 975, .y = 70 }, Block{ .x = 1205, .y = 70 }, Block{ .x = 1435, .y = 70 }, Block{ .x = 1665, .y = 70 } }, [8]Block{ Block{ .x = 55, .y = 110 }, Block{ .x = 285, .y = 110 }, Block{ .x = 515, .y = 110 }, Block{ .x = 745, .y = 110 }, Block{ .x = 975, .y = 110 }, Block{ .x = 1205, .y = 110 }, Block{ .x = 1435, .y = 110 }, Block{ .x = 1665, .y = 110 } }, [8]Block{ Block{ .x = 55, .y = 150 }, Block{ .x = 285, .y = 150 }, Block{ .x = 515, .y = 150 }, Block{ .x = 745, .y = 150 }, Block{ .x = 975, .y = 150 }, Block{ .x = 1205, .y = 150 }, Block{ .x = 1435, .y = 150 }, Block{ .x = 1665, .y = 150 } }, [8]Block{ Block{ .x = 55, .y = 190 }, Block{ .x = 285, .y = 190 }, Block{ .x = 515, .y = 190 }, Block{ .x = 745, .y = 190 }, Block{ .x = 975, .y = 190 }, Block{ .x = 1205, .y = 190 }, Block{ .x = 1435, .y = 190 }, Block{ .x = 1665, .y = 190 } }, [8]Block{ Block{ .x = 55, .y = 230 }, Block{ .x = 285, .y = 230 }, Block{ .x = 515, .y = 230 }, Block{ .x = 745, .y = 230 }, Block{ .x = 975, .y = 230 }, Block{ .x = 1205, .y = 230 }, Block{ .x = 1435, .y = 230 }, Block{ .x = 1665, .y = 230 } }, [8]Block{ Block{ .x = 55, .y = 270 }, Block{ .x = 285, .y = 270 }, Block{ .x = 515, .y = 270 }, Block{ .x = 745, .y = 270 }, Block{ .x = 975, .y = 270 }, Block{ .x = 1205, .y = 270 }, Block{ .x = 1435, .y = 270 }, Block{ .x = 1665, .y = 270 } }, [8]Block{ Block{ .x = 55, .y = 310 }, Block{ .x = 285, .y = 310 }, Block{ .x = 515, .y = 310 }, Block{ .x = 745, .y = 310 }, Block{ .x = 975, .y = 310 }, Block{ .x = 1205, .y = 310 }, Block{ .x = 1435, .y = 310 }, Block{ .x = 1665, .y = 310 } } };
+
+    return LevelState{ .paddle = paddle, .ball = ball, .keyboard = keyboard_state, .level = blocks };
 }
 
 fn render_borders(renderer: ?*sdl.SDL_Renderer) void {
@@ -149,39 +160,7 @@ pub fn main() !void {
     }
     defer sdl.SDL_DestroyRenderer(renderer);
 
-    // TODO: Replace all of this with a gamestate singleton
-    var paddle = Paddle{};
-    var ball = Ball{ .vel_y = 10 };
-    var keyboard_state = KeyboardState{};
-
-    // TODO: Replace this with an 8x8 grid, which can optionally have a block inside.
-    var blocks = std.ArrayList(Block).init(std.heap.page_allocator);
-    defer blocks.deinit();
-    blocks.append(Block{ .x = 55, .y = 30 }) catch |err| {
-        return err;
-    };
-    blocks.append(Block{ .x = 285, .y = 30 }) catch |err| {
-        return err;
-    };
-    blocks.append(Block{ .x = 515, .y = 30 }) catch |err| {
-        return err;
-    };
-    blocks.append(Block{ .x = 745, .y = 30 }) catch |err| {
-        return err;
-    };
-    blocks.append(Block{ .x = 975, .y = 30 }) catch |err| {
-        return err;
-    };
-    blocks.append(Block{ .x = 1205, .y = 30 }) catch |err| {
-        return err;
-    };
-    blocks.append(Block{ .x = 1435, .y = 30 }) catch |err| {
-        return err;
-    };
-    blocks.append(Block{ .x = 1665, .y = 30 }) catch |err| {
-        return err;
-    };
-
+    var game_state = init_game_state();
     var running = true;
 
     const freq = @as(f64, @floatFromInt(sdl.SDL_GetPerformanceFrequency()));
@@ -194,59 +173,63 @@ pub fn main() !void {
         last_frame = curr_frame;
         var event: sdl.SDL_Event = undefined;
         while (sdl.SDL_PollEvent(&event) > 0) {
-            const should_close = handle_event(event, &keyboard_state);
+            const should_close = handle_event(event, &game_state.keyboard);
             if (should_close) {
                 running = false;
             }
         }
 
-        if (keyboard_state.left.is_down) {
-            paddle.vel_x -= 30 * dt;
+        if (game_state.keyboard.left.is_down) {
+            game_state.paddle.vel_x -= 30 * dt;
         }
-        if (keyboard_state.right.is_down) {
-            paddle.vel_x += 30 * dt;
-        }
-
-        paddle.x += @as(i32, @intFromFloat(paddle.vel_x));
-
-        if (paddle.vel_x > paddle.max_vel) {
-            paddle.vel_x = paddle.max_vel;
-        } else if (paddle.vel_x < -paddle.max_vel) {
-            paddle.vel_x = -paddle.max_vel;
+        if (game_state.keyboard.right.is_down) {
+            game_state.paddle.vel_x += 30 * dt;
         }
 
-        if (paddle.vel_x > 0) {
-            paddle.vel_x -= 10 * dt;
-        } else if (paddle.vel_x < 0) {
-            paddle.vel_x += 10 * dt;
-        }
-        if (paddle.x < paddle.left_bound) {
-            paddle.x = paddle.left_bound;
-            paddle.vel_x = -paddle.vel_x;
-        } else if (paddle.x > paddle.right_bound) {
-            paddle.x = paddle.right_bound;
-            paddle.vel_x = -paddle.vel_x;
+        game_state.paddle.x += @as(i32, @intFromFloat(game_state.paddle.vel_x));
+
+        if (game_state.paddle.vel_x > game_state.paddle.max_vel) {
+            game_state.paddle.vel_x = game_state.paddle.max_vel;
+        } else if (game_state.paddle.vel_x < -game_state.paddle.max_vel) {
+            game_state.paddle.vel_x = -game_state.paddle.max_vel;
         }
 
-        ball.x += @as(i32, @intFromFloat(ball.vel_x));
-        ball.y += @as(i32, @intFromFloat(ball.vel_y));
+        if (game_state.paddle.vel_x > 0) {
+            game_state.paddle.vel_x -= 10 * dt;
+        } else if (game_state.paddle.vel_x < 0) {
+            game_state.paddle.vel_x += 10 * dt;
+        }
+        if (game_state.paddle.x < game_state.paddle.left_bound) {
+            game_state.paddle.x = game_state.paddle.left_bound;
+            game_state.paddle.vel_x = -game_state.paddle.vel_x;
+        } else if (game_state.paddle.x > game_state.paddle.right_bound) {
+            game_state.paddle.x = game_state.paddle.right_bound;
+            game_state.paddle.vel_x = -game_state.paddle.vel_x;
+        }
 
-        const should_close = test_border_collision(&ball);
+        game_state.ball.x += @as(i32, @intFromFloat(game_state.ball.vel_x));
+        game_state.ball.y += @as(i32, @intFromFloat(game_state.ball.vel_y));
+
+        const should_close = test_border_collision(&game_state.ball);
         if (should_close) {
             running = false;
         }
-        test_paddle_collision(&ball, &paddle);
-        for (0..blocks.items.len) |i| {
-            test_block_collision(&ball, &blocks.items[i]);
+        test_paddle_collision(&game_state.ball, &game_state.paddle);
+        for (0..8) |x| {
+            for (0..8) |y| {
+                test_block_collision(&game_state.ball, &game_state.level[x][y]);
+            }
         }
 
         _ = sdl.SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
         _ = sdl.SDL_RenderClear(renderer);
         render_borders(renderer);
-        render_paddle(renderer, &paddle);
-        render_ball(renderer, &ball);
-        for (0..blocks.items.len) |i| {
-            render_block(renderer, &blocks.items[i]);
+        render_paddle(renderer, &game_state.paddle);
+        render_ball(renderer, &game_state.ball);
+        for (0..8) |x| {
+            for (0..8) |y| {
+                render_block(renderer, &game_state.level[x][y]);
+            }
         }
         _ = sdl.SDL_RenderPresent(renderer);
 
